f5b75bb3a49686272ba90947b26ade1a183900a6
Shreyansh1912 <98804905+Shreyansh1912@users.noreply.github.com>
2023-04-07 14:50:00 +0530

    firebase

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Fireauth from './Components/Fireauth';
+import Firebase from './Components/Firebase';
+import Firestorage from './Components/Firestorage';
+function App() {
+  return (
+      <>
+      <Fireauth />
+      {/* <Firebase /> */}
+      <Firestorage />
+      </>
+  );
+}
+
+export default App;
